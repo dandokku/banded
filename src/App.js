e87aa3e51a36f9579383c911137e419042620957
Dandokku <jesulobadaniel1@gmail.com>
2024-01-10 16:45:28 +0100

    theme

--- src/App.js
+++ src/App.js
@@ -4,15 +4,30 @@ import Home from "scenes/home/Home"
 import Login from "scenes/login/Login"
 import Profile from "scenes/profile/Profile"
 
+import { useMemo } from "react"
+import { useSelector } from "react-redux"
+import { ThemeProvider } from "@mui/material"
+import CssBaseLine from "@mui/material/CssBaseline" 
+import {createTheme } from "@mui/material/styles"
+import { themeSettings } from "theme"
+
+
 function App() {
+  const mode = useSelector((state) => state.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/" element={<Profile />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+           {/* this resets css back to normal */}
+          <CssBaseLine />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Profile />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
